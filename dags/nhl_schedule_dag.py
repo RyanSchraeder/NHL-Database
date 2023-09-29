8be7f07ae69bae6bd2ab8ec7c6710802a67a29c2
Ryan Schraeder <rschraeder2@gmail.com>
2023-09-29 14:51:17 -0600

    update dag for season data

--- dags/nhl_schedule_dag.py
+++ dags/nhl_schedule_dag.py
@@ -1,65 +1,20 @@
 import airflow
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator as ECSOperator
 from datetime import datetime, timedelta
 import pendulum
-from snowflake_transfer import SnowflakeIngest
-from snowflake_queries import (
-    snowflake_stages,
-    snowflake_cleanup,
-    snowflake_schema,
-    snowflake_ingestion
-)
+
 
 # Environment Variable
 from airflow.models import Variable
 
 local_tz = pendulum.timezone('America/Denver')
 subnet = 'subnet-086f7f66dfd373f93'
-logs_group = 'airflow-Test-Task'
-
-# Set up class and ingest data from Raw Source to S3
-execute = SnowflakeIngest()
-
-
-@task(task_id="nhl_extract_transform")
-def nhl_extract_transform(source, endpoint, year, s3_bucket_name, snowflake_conn):
-
-    # Set up the module
-    run = execute(source, endpoint, year, s3_bucket_name, snowflake_conn)
-
-    # Extract raw data and transform
-    output_df = run.file_parser()
-
-    # Load transformed data to S3
-    run.s3_parser(output_df)
-
-
-@task(task_id="snowflake_setup")
-def snowflake_setup(source, endpoint, year, s3_bucket_name, snowflake_conn):
-
-    # Set up the module
-    run = execute(source, endpoint, year, s3_bucket_name, snowflake_conn)
-
-    # CREATE STAGES FROM S3 -> SNOWFLAKE
-    run.snowflake_query_exec(queries=snowflake_stages)
-
-    # CREATE SCHEMA
-    run.snowflake_query_exec(queries=snowflake_schema())
-
-
-@task(task_id="snowflake_ingest")
-def snowflake_ingest(source, endpoint, year, s3_bucket_name, snowflake_conn):
-
-    # Set up the module
-    run = execute(source, endpoint, year, s3_bucket_name, snowflake_conn)
-
-    # DEDUPE DATA
-    run.snowflake_query_exec(queries=snowflake_cleanup(year))
-
-    # INGEST DATA FROM S3
-    run.snowflake_query_exec(queries=snowflake_ingestion())
+logs_group = 'airflow-nhl-pipeline-airflow-env-Task'
+cluster = 'nhl-data-pipeline'
+task_definition = 'airflow-data-pipelines:5'
+container_name = 'airflow-data-pipelines'
 
 
 with DAG(
@@ -74,50 +29,20 @@ with DAG(
     start_date=datetime.now(),
     catchup=False
 ) as dag:
-    nhl_extract_transform = PythonOperator(
-        task_id="nhl_extract_transform",
+    nhl_seasons = ECSOperator(
+        task_id="nhl_seasons",
+        cluster=f"{cluster}",
+        task_definition=f"{task_definition}",
         launch_type="FARGATE",
+        overrides={
+            "containerOverrides": [
+                {
+                    "name": f"{container_name}",
+                    "command": [
+                        f"python3 src/scripts/snowflake_transfer.py seasons https://www.hockey-reference.com/leagues/ 2023 nhl-data-raw standard production"]
+                }
+            ],
+        },
         network_configuration={'awsvpcConfiguration': {'assignPublicIp': 'ENABLED', 'subnets': [f'{subnet}']}},
-        awslogs_group=f"{logs_group}",
-        python_callable=nhl_extract_transform,
-        op_kwargs={
-            "source": "seasons",
-            "endpoint": "https://www.hockey-reference.com/leagues/",
-            "year": datetime.now().year,
-            "s3_bucket_name": "nhl-data-raw",
-            "snowflake_conn": "standard"
-        }
+        awslogs_group=f"{logs_group}"
     )
-
-    snowflake_setup = PythonOperator(
-        task_id="snowflake_setup",
-        launch_type="FARGATE",
-        network_configuration={'awsvpcConfiguration': {'assignPublicIp': 'ENABLED', 'subnets': [f'{subnet}']}},
-        awslogs_group=f"{logs_group}",
-        python_callable=snowflake_setup,
-        op_kwargs={
-            "source": "seasons",
-            "endpoint": "https://www.hockey-reference.com/leagues/",
-            "year": datetime.now().year,
-            "s3_bucket_name": "nhl-data-raw",
-            "snowflake_conn": "standard"
-        }
-    )
-
-    snowflake_ingest = PythonOperator(
-        task_id="snowflake_setup",
-        launch_type="FARGATE",
-        network_configuration={'awsvpcConfiguration': {'assignPublicIp': 'ENABLED', 'subnets': [f'{subnet}']}},
-        awslogs_group=f"{logs_group}",
-        python_callable=snowflake_ingest,
-        op_kwargs={
-            "source": "seasons",
-            "endpoint": "https://www.hockey-reference.com/leagues/",
-            "year": datetime.now().year,
-            "s3_bucket_name": "nhl-data-raw",
-            "snowflake_conn": "standard"
-        }
-    )
-
-    # Dependencies
-    nhl_extract_transform >> snowflake_setup >> snowflake_ingest
