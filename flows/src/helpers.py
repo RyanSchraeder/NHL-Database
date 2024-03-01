526403694d00341bf9362734a433bf11cd17e8cd
Ryan Schraeder <rschraeder2@gmail.com>
2024-03-01 09:56:34 -0700

    add snowflake query exec to helpers

--- flows/src/helpers.py
+++ flows/src/helpers.py
@@ -1,4 +1,17 @@
-import os 
+import time
+import sys
+import os
+
+# For casting query results to a Pandas DataFrame
+import pandas as pd
+
+# Prefect logger
+from prefect import get_run_logger
+
+# Snowflake Connections
+from snowflake.connector import ProgrammingError
+from prefect_snowflake import SnowflakeCredentials, SnowflakeConnector
+from src.connectors import get_snowflake_connection
 
 def setup(
     source: str,
@@ -10,7 +23,8 @@ def setup(
       :param: endpoint -> base url that is passed to the pipeline via the endpoint parameter. Default: https://www.hockey-reference.com/leagues/
       :year: year -> year of which to process data from. Default: current date at runtime
   """
-    print(f'Received endpoint {endpoint} for source {source} and year {year}.')
+    logging = get_run_logger()
+    logging.info(f'Received endpoint {endpoint} for source {source} and year {year}.')
     
     # Build endpoint URL & Filenames
     paths = ('seasons', 'playoffs', 'teams')
@@ -27,8 +41,75 @@ def setup(
         else:
             pass
     else:
-        print(f'Invalid source specified: {source}')
+        logging.error(f'Invalid source specified: {source}')
         sys.exit(1)
 
-    print(f"URL Built: {url}\nDestination Filename: {filename}")
+    logging.info(f"URL Built: {url}\nDestination Filename: {filename}")
     return url, filename
+
+
+def snowflake_query_exec(queries, method: str = 'standard'):
+    logging = get_run_logger()
+    try:
+        # Cursor & Connection
+        conn = get_snowflake_connection(method)
+        logging.info(f"Snowflake connection established: {conn}")
+        
+        response = {}
+        
+        if conn:
+            curs = conn.cursor()
+        
+            # Retrieve formatted queries and execute - Snowflake Connector Form. Async
+            for idx, query in queries.items():
+                curs.execute_async(query)
+                query_id = curs.sfqid
+                logging.info(f'Query added to queue: {query_id}')
+        
+                curs.get_results_from_sfqid(query_id)
+        
+                # IF THE SNOWFLAKE QUERY RETURNS DATA, STORE IT. ELSE, CONTINUE PROCESS.
+                result = curs.fetchone()
+                df = curs.fetch_pandas_all()
+        
+                if result:
+                    logging.info(f'Query completed successfully and stored: {query_id}')
+                    response[idx] = result[0]
+                    if len(df):
+                        return df
+        
+                while conn.is_still_running(conn.get_query_status_throw_if_error(query_id)):
+                    logging.info(f'Awaiting query completion for {query_id}')
+                    time.sleep(1)
+        
+                return response
+        
+        if not conn:
+            # Retrieve formatted queries and execute - Fallback: Prefect Snowflake Connector. Sync
+            # Prefect Snowflake Connector
+            logging.warning(f"Snowflake cursor is empty! Attempting Prefect Connector.")
+            credentials = SnowflakeCredentials.load("development")
+            
+            with SnowflakeConnector.load("development") as cnx:
+                for idx, query in queries.items():
+                    while True:
+                        
+                        logging.info(
+                            f"""
+                            Executing Query: \n'
+                            \t\t{query}\n
+                            """
+                        )
+                        result = cnx.fetch_one(query)
+                        # full_result = cnx.fetch_all()
+                        
+                        logging.info(f'Query Result from Prefect Snowflake: {result}')
+                        
+                        if result:
+                            logging.info(f'Query completed successfully and stored: {query}')
+                            response[idx] = result[0]
+                            if len(result):
+                                return result
+
+    except ProgrammingError as err:
+        logging.error(f'Programming Error: {err}')
